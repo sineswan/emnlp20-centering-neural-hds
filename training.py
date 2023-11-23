be52a7f3950cb368a10b9d2f655db284f70b0926
wan049 <Stephen.Wan@csiro.au>
2023-11-23 16:06:11 +0100

    adding code in progress (logging for adjacency matrices)

--- training.py
+++ training.py
@@ -168,6 +168,36 @@ def update_cp_seq(len_seq, len_sents, config, cur_cp_ind, text_inputs, cp_seq_li
 
     return cp_seq_list
 
+
+def update_adjmat_seq(len_seq, len_sents, config, adjmat, text_inputs, adjmat_seq_list):
+    sent_mask = torch.sign(len_sents)  # (batch_size, len_sent)
+    sent_mask = utils.cast_type(sent_mask, FLOAT, config.use_gpu)
+    num_sents = sent_mask.sum(dim=1)  # (batch_size)
+    for batch_ind, cur_doc_cp in enumerate(cur_cp_ind):
+        cur_doc_len = int(len_seq[batch_ind])
+        cur_sent_len = len_sents[batch_ind]
+        cur_sent_num = int(num_sents[batch_ind])  # (max_sent_num)
+        end_loc = 0
+        prev_loc = 0
+
+        cur_cp_seq = []
+        cur_text_seq = text_inputs[batch_ind]
+        for cur_sent_ind in range(cur_sent_num):
+            end_loc += int(cur_sent_len[cur_sent_ind])
+            cur_sent_seq = cur_text_seq[prev_loc:end_loc]  # get the current sentence seq from a doc
+
+            cur_sent_cp = cur_doc_cp[cur_sent_ind]
+            cur_cp_seq.append(int(cur_sent_seq[cur_sent_cp]))  # get seq id of Cp
+
+            prev_loc = end_loc
+        # end for
+        adjmat_seq_list.append(cur_cp_seq)
+
+    # end for
+
+    return adjmat_seq_list
+
+
 #
 def train(model, optimizer, scheduler, dataset_train, dataset_valid, dataset_test, config, evaluator):  # valid_feed can be None
 
@@ -216,6 +246,7 @@ def train(model, optimizer, scheduler, dataset_train, dataset_valid, dataset_tes
         num_sents_list = []
 
         cp_seq_list = []
+        adjmat_seq_list = []
 
         for text_inputs, label_y, *remains in dataloader_train:
             mask_input = remains[0]
@@ -280,6 +311,7 @@ def train(model, optimizer, scheduler, dataset_train, dataset_valid, dataset_tes
                 num_sents_list = num_sents_list + model_outputs[5]
 
                 cp_seq_list = update_cp_seq(len_seq, len_sents, config, model_outputs[4], text_inputs, cp_seq_list)
+                adjmat_seq_list = update_adjmat_seq(len_seq, len_sents, config, model_outputs[1], text_inputs, adjmat_seq_list)
 
             # ##########
             # ## validation
@@ -364,12 +396,12 @@ def train(model, optimizer, scheduler, dataset_train, dataset_valid, dataset_tes
         ## convert Cp indexes in sentences to sequence number
         if config.gen_logs: # and config.target_model.lower() == "cent_attn":
             cp_log_name = "log_cp_" + config.corpus_target.lower() + "_" + str(config.essay_prompt_id_train) + ".log"
-            col = ["tid", "doc_cp_seq"]
+            col = ["tid", "doc_cp_seq", "adj_mat"]
             with open(os.path.join(config.session_dir, cp_log_name), "w") as log_file:
                 print(f"session_log file: {os.path.join(config.session_dir, cp_log_name)}")
                 writer = csv.DictWriter(log_file, fieldnames=col)
                 for i in range(len(cp_seq_list)):
-                    writer.writerow({'tid': tid_list[i], 'doc_cp_seq': cp_seq_list[i]})
+                    writer.writerow({'tid': tid_list[i], 'doc_cp_seq': cp_seq_list[i], 'adj_mat': adjmat_seq_list[i]})
 
         # end if
 
